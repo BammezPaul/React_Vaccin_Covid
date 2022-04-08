f4ea512ed022dee11bd2f9d5a9dd35dd2e0990f3
PAUL BAMMEZ <bammezpaul@gmail.com>
2022-04-08 08:25:43 +0200

    correctif

--- src/components/rendez_vous.js
+++ src/components/rendez_vous.js
@@ -1,85 +1,19 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { StyleSheet, View, Text, TextInput, ScrollView, Pressable } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
-class rendez_vous extends Component {
-
-    state = {
-        nom: "",
-        prenom: "",
-    }
-
-    render() {
-        return(
-            <KeyboardAwareScrollView>
-                <ScrollView style={styles.content}>
-                    <View style={styles.titreSection}>
-                        <Text style={styles.titre}>Vous voulez nous partager un bon restaurant que vous déconseilleriez ?</Text>
-                    </View>
-                    <Text style={styles.alert}>
-                        {this.state.message}
-                    </Text>
-                    <View>
-                        <Text>Nom :</Text>
-                        <TextInput style={styles.input}
-                            value = {this.state.nom}
-                            onChangeText={(nom)=> this.setState({ nom: nom })} 
-                        />
-                    </View> 
-                    <View>
-                        <Text>Prénom :</Text>
-                        <TextInput style={styles.input}
-                            value = {this.state.prenom}
-                            onChangeText={(prenom)=> this.setState({ prenom: prenom })} 
-                        />
-                    </View>
-                </ScrollView>
-            </KeyboardAwareScrollView>
-        );
-    }
+export default class rendez_vous extends React.Component {
+  render() {
+    return (
+      <Text>
+          test
+      </Text>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    content: {
-        marginTop:50,
-        marginHorizontal:10
-    },
-    titreSection: {
-        
-        marginBottom:20,
-        borderWidth:1,
-        borderRadius:10,
-        padding:10
-        
-    },
-    titre: {
-        fontSize:20,
-        textAlign:'center',
-        fontWeight:'bold'
-    },
-    alert: {
-        color:'red',
-        textAlign:'center',
-        marginBottom:20
-    },
-    input: {
-        padding: 2,
-        margin:10,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderColor: 'black',
-    },
-    button: {
-        backgroundColor:'blue',
-        marginHorizontal:130,
-        borderRadius:6,
-        paddingVertical:10
-    },
-    textButton: {
-        color:'white',
-        textAlign:'center'
-    }   
+    background: {
+      width: '100%',
+      height: '100%'
+    }
 });
-
-export default ContactForm;
